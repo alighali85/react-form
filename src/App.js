091abf3e38ba8280514427192390b3d22023a277
Ali Ghali <alifriends@live.com>
2020-07-14 13:07:24 +0200

    feat(containers): place and use form container in the app

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import SupportForm from './containers/suportForm/SupportForm';
 import AppHeader from './components/appHeader'
 import { LONDON_WEATHER, APP_TITLE } from './config/main';
+import {Container, Grid} from '@material-ui/core'
 
 function App() {
   return (
     <div className="App">
       <AppHeader weatherApi={LONDON_WEATHER} title={APP_TITLE}/>
       <Container>
-      <SupportForm/>
+        <Grid container>
+          <SupportForm/>
+        </Grid>
+      </Container>
     </div>
   );
 }
---
+++ src/containers/suportForm/SupportForm.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+
+export default function SupportForm({name ='name'}) {
+    return (
+        <div> here is the form</div>
+    )
+}
