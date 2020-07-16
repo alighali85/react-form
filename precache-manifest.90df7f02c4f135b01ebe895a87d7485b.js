ebd49a88c7ef45e8106d2f296f8ad3c98d51e8eb
Ali Ghali <alifriends@live.com>
2020-07-16 20:23:57 +0200

    Updates

---
+++ precache-manifest.90df7f02c4f135b01ebe895a87d7485b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ae74ffccd457daddb4b3637b774645a",
+    "url": "/react-form/index.html"
+  },
+  {
+    "revision": "890e1d24e4e1bed97753",
+    "url": "/react-form/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a2e65c998328675c7dcd",
+    "url": "/react-form/static/js/2.f2ce6b93.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-form/static/js/2.f2ce6b93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "890e1d24e4e1bed97753",
+    "url": "/react-form/static/js/main.8b4be772.chunk.js"
+  },
+  {
+    "revision": "7fa4f514a1846fc2fd76",
+    "url": "/react-form/static/js/runtime-main.f8f393fb.js"
+  }
+]);
